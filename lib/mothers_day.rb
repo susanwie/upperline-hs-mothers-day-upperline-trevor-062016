2fc75c55826136b8ed32bf8c3735a581a3060a5c
Susan Wie <susanhwie@gmail.com>
2016-06-21 17:20:16 +0000

    Done.

--- lib/mothers_day.rb
+++ lib/mothers_day.rb
@@ -1,6 +1,9 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
+puts "Who are you writing the card for?"
+name = gets.chomp
 
+def mothers_day(name)
+  puts "Happy Mothers Day, #{name}!"
+end
 
-
-## Call your method below - use puts to see it in the terminal!
-
+mothers_day(name)
